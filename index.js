dee5b1b6ac4f8c9a96c2044d418d845ba6639dc1
SanjaySodani <88200484+SanjaySodani@users.noreply.github.com>
2021-10-02 18:35:22 +0530

    Update index.js

--- index.js
+++ index.js
@@ -5,54 +5,55 @@ window.onload = function () {
     button.addEventListener("click", getData);
 
     async function getData() {
-        let response = await fetch("https://api.openbrewerydb.org/breweries");
-        if (response.ok) {
-            let data = await response.json();
-            myDiv.innerHTML = "";
-            for (let i of data) {
-                let na = `<span class="text-muted">Not available</span>`;
-                let address = na;
-                if (i.street !== null) {
-                    address = i.street;
-                    if (i.address_2 !== null) {
-                        address = ", " + i.address_2
+        try {
+            let response = await fetch("https://api.openbrewerydb.org/breweries");
+            if (response.ok) {
+                let data = await response.json();
+                myDiv.innerHTML = "";
+                for (let i of data) {
+                    let na = `<span class="text-muted">Not available</span>`;
+                    let address = na;
+                    if (i.street !== null) {
+                        address = i.street;
+                        if (i.address_2 !== null) {
+                            address = ", " + i.address_2
+                        }
+                        if (i.address_2 !== null) {
+                            address = ", " + i.address_2
+                        }
                     }
-                    if (i.address_2 !== null) {
-                        address = ", " + i.address_2
+
+                    let city = na;
+                    if (i.city !== null) {
+                        city = i.city;
                     }
-                }
 
-                let city = na;
-                if (i.city !== null) {
-                    city = i.city;
-                }
+                    let state = na;
+                    if (i.state !== null) {
+                        state = i.state;
+                    }
 
-                let state = na;
-                if (i.state !== null) {
-                    state = i.state;
-                }
+                    let postal_code = na;
+                    if (i.postal_code !== null) {
+                        postal_code = i.postal_code;
+                    }
 
-                let postal_code = na;
-                if (i.postal_code !== null) {
-                    postal_code = i.postal_code;
-                }
+                    let country = na;
+                    if (i.country !== null) {
+                        country = i.country;
+                    }
 
-                let country = na;
-                if (i.country !== null) {
-                    country = i.country;
-                }
+                    let website_url = na;
+                    if (i.website_url !== null) {
+                        website_url = `<a href="${i.website_url}" class="card-link">${i.website_url}</a>`
+                    }
 
-                let website_url = na;
-                if (i.website_url !== null) {
-                    website_url = `<a href="${i.website_url}" class="card-link">${i.website_url}</a>`
-                }
+                    let phone = na;
+                    if (i.phone !== null) {
+                        phone = i.phone;
+                    }
 
-                let phone = na;
-                if (i.phone !== null) {
-                    phone = i.phone;
-                }
-
-                myDiv.innerHTML += `<div class="col my-2"><div class="card myCard shadow">
+                    myDiv.innerHTML += `<div class="col my-2"><div class="card myCard shadow">
                         <div class="card-body">
                         <h5 class="card-title">${i.name}</h5>
                         <h6 class="card-subtitle mb-3 text-muted">Brewery type: ${i.brewery_type}</h6>
@@ -65,9 +66,13 @@ window.onload = function () {
                         <p class="card-text mb-1">Phone: ${phone}</p>
                         </div>
                     </div></div>`
+                }
+            }
+            else {
+                throw new Error("Error");
             }
         }
-        else {
+        catch (err) {
             myDiv.innerHTML = `<h3 class="font-weight-normal text-danger text-center">An error occurred!</h3>`;
         }
     }
